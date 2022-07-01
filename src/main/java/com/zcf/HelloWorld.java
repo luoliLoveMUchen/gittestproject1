7f2a649a2a77a4bfe5e2fd90b6dd179113555e4b
zcf <1574487841@qq.com>
2022-07-01 10:29:14 +0800

    第一次提交代码

---
+++ src/main/java/com/zcf/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zcf;
+
+public class HelloWorld {
+
+    /**
+     * idea 测试git 管控代码
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
